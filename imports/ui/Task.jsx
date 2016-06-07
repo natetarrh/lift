6329081bc9a17e93d01933d06b8e418847d6b67e
Nate Tarrh <nate@natetarrh.com>
2016-06-06 22:25:26 -0400

    Fix indent

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -17,16 +17,16 @@ export default class Task extends Component {
     const taskClassName = this.props.task.checked ? 'checked' : '';
     return (
       <li className={taskClassName}>
-      <button className="delete" onClick={this.deleteThisTask.bind(this)}>
-        &times;
-      </button>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
 
-      <input
-        type="checkbox"
-        readOnly
-        checked={this.props.task.checked}
-        onClick={this.toggleChecked.bind(this)}
-      />
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
 
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
