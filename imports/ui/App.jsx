7471d38a0795642af0116141912009736066c406
Nate Tarrh <nate@natetarrh.com>
2016-06-06 23:25:37 -0400

    Add ability to make tasks private

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -37,9 +37,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
@@ -86,6 +95,8 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
